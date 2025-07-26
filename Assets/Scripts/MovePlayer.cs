bb4a6947231133d76d74404be7c7a3b3798dc6c9
DzaneQ <lucker1996@gmail.com>
2025-07-26 17:17:30 +0200

    fix: Player moves towards cursor correctly

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    private const float Speed = 1f;
+    private const float Speed = 10f;
     private Camera cam;
+    private float _cameraDistance;
 
     void Start()
     {
         cam = Camera.main;
+        _cameraDistance = cam.transform.position.y; // Distance between the camera and the plane (for plane y = 0)
     }
 
     void Update()
     {
-        Vector3 playerScreenPosition = cam.WorldToScreenPoint(transform.position);
+        MoveTowardsCursor();
+    }
+
+    private void MoveTowardsCursor()
+    {
         Vector3 inputPosition = Input.mousePosition;
-        //Vector3 inputWorldPosition = cam.ScreenToWorldPoint(inputPosition);
-        //Debug.Log($"Input position: {inputWorldPosition.x}, {inputWorldPosition.y}, {inputWorldPosition.z}");
-        Debug.Log($"Player position: {transform.position.x}, {transform.position.y}, {transform.position.z}");
-        //Vector3 targetPosition = new(inputWorldPosition.x, transform.position.y, inputWorldPosition.z);
-        //transform.position = cam.ScreenToWorldPoint(Vector3.MoveTowards(cam.ScreenToWorldPoint(playerScreenPosition), targetPosition, Speed * Time.deltaTime));
+        inputPosition.z = _cameraDistance;
+        Vector3 inputWorldPosition = cam.ScreenToWorldPoint(inputPosition);
+        Vector3 targetPosition = new(inputWorldPosition.x, transform.position.y, inputWorldPosition.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Speed * Time.deltaTime);
     }
 }
